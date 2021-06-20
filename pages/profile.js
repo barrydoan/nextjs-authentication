a5b3fc0a729368dbe74fb5928e8524301fc32ee3
Barry Doan <barry.n.doan@gmail.com>
2021-06-20 08:44:03 -0400

    server side pages guard

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,26 @@
+import {getSession} from "next-auth/client";
+
 import UserProfile from '../components/profile/user-profile';
 
 function ProfilePage() {
   return <UserProfile />;
+}
+
+export async function getServerSideProps(contex) {
+  const session = await getSession({req: contex.req});
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false
+      }
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
 
 export default ProfilePage;
